33a54aac0620dc797230687e6dd38a3366f77366
crazy-learner-xyz <73959442+crazy-learner-xyz@users.noreply.github.com>
2020-11-07 09:10:35 -0800

    Was able to pass, but time limit exceeded

--- 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/solution.py
+++ 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/solution.py
@@ -3,19 +3,20 @@ class Solution(object):
     	l, r = 0, 0
     	maxval = nums[0]
     	minval = nums[0]
-    	max_len = INF_MIN
-    	while r <= len(nums)-1:
-    		if abs(nums[r+1] - maxval) > limit or abs(nums[r+1] - minval) > limit:
-    			r += 1
-    			while true:
-    				l+=1
-    				maxval = max(nums[l:r+1])
-    				minval = min(nums[l:r+1])
-    				if abs(nums[r+1] - maxval) <= limit or abs(nums[r+1] - minval) <= limit:
-    					break
-    			max_len = r - l + 1
-    		else:
-    			r += 1
-    			max_len = max(max_len, r-l+1)
+    	max_len = 1
+    	while r <= len(nums)-2:
+            if abs(nums[r+1] - maxval) > limit or abs(nums[r+1] - minval) > limit:
+                r += 1
+                maxval = max(nums[l:r+1])
+                minval = min(nums[l:r+1])
+                while abs(nums[r] - maxval) > limit or abs(nums[r] - minval) > limit:
+                    l+=1
+                    maxval = max(nums[l:r+1])
+                    minval = min(nums[l:r+1])
+            else:
+                r += 1
+                #print(l,r)
+                max_len = max(max_len, r-l+1)
+                maxval = max(maxval, nums[r])
+                minval = min(minval, nums[r])
     	return max_len
-
