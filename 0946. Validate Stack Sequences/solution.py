952c0f9728f1ca3961007775b83470b1a7386790
crazy-learner-xyz <73959442+crazy-learner-xyz@users.noreply.github.com>
2020-11-13 12:24:47 -0800

    Update solution.py

--- 0946. Validate Stack Sequences/solution.py
+++ 0946. Validate Stack Sequences/solution.py
@@ -5,4 +5,25 @@ class Solution(object):
         :type popped: List[int]
         :rtype: bool
         """
-        
+        stack = []
+        while pushed != [] or stack != []: #rethink about this termination condition.
+            print(stack)
+            if pushed[0] != popped[0]:
+                a = pushed.pop(0)
+                stack.append(a)
+                
+            else:
+                pushed.pop(0)
+                popped.pop(0)
+
+        while popped != []:
+            if stack[-1] != popped[0]:
+                return False
+            else:
+                popped.pop(0)
+                stack.pop(-1)
+                
+        if popped != []:
+            return False
+        else:
+            return True
