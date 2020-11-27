a4d5976d951538c053e6e6f98e4c8476ad5833ba
crazy-learner-xyz <73959442+crazy-learner-xyz@users.noreply.github.com>
2020-11-27 11:50:57 -0800

    Add files via upload

---
+++ 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minSumOfLengths(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+
