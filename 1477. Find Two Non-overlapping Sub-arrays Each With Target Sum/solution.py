27e41854e8b6051e50f5de3e95407c3d7c8e5e7a
crazy-learner-xyz <73959442+crazy-learner-xyz@users.noreply.github.com>
2020-11-27 17:26:02 -0800

    Update solution.py

--- 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum/solution.py
+++ 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum/solution.py
@@ -5,5 +5,43 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
+        prefix = [len(arr)+1] * len(arr) # initialized to an unreachable number
+        lst = []
+        for i in range(len(arr)): 
+            while lst and sum(lst)+arr[i] > target:
+                lst.pop(0)
+            lst.append(arr[i])
 
+            if sum(lst) == target:
+                if i == 0:
+                    prefix[i] = len(lst)
+                else:
+                    prefix[i] = min(prefix[i-1], len(lst))
+            elif i!= 0: #if i=0, then default
+                prefix[i] = prefix[i-1]
+        prefix = [len(arr)+1] + prefix[:-1]
+        
+        lst = []
+        suffix = [len(arr)+1] * len(arr)
+        for i in range(len(arr)-1, 0, -1): 
+            while lst and  sum(lst)+arr[i] > target:
+                lst.pop(0)
+            lst.append(arr[i])
+
+            if sum(lst) == target:
+                if i == len(arr)-1:
+                    suffix[i] = len(lst)
+                else:
+                    suffix[i] = min(suffix[i+1], len(lst))
+            elif i != len(arr)-1:
+                suffix[i] = suffix[i+1]
+
+        print("prefix")
+        print(prefix)
+        print("suffix")
+        print(suffix)
+        two_arrays = [prefix[i] + suffix[i] for i in range(len(suffix))]
+        if min(two_arrays) > len(arr):
+            return -1
+        else:
+            return min(two_arrays)
