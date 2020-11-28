d19ca6b83704e218ccb522b999ad7096d05b6822
crazy-learner-xyz <73959442+crazy-learner-xyz@users.noreply.github.com>
2020-11-27 18:12:01 -0800

    Update solution.py

--- 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum/solution.py
+++ 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum/solution.py
@@ -5,41 +5,50 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        prefix = [len(arr)+1] * len(arr) # initialized to an unreachable number
+        diction = {}
+        diction2 = {}
+        s = 0
+        s_arr = sum(arr)
+        for i in range(len(arr)):
+            s += arr[i]
+            diction[s] = i
+            diction2[s] = s_arr - s
+        
+        
+        prefix = [float('inf')] * len(arr) # initialized to an unreachable number
         lst = []
+        s_lst = 0
         for i in range(len(arr)): 
-            while lst and sum(lst)+arr[i] > target:
-                lst.pop(0)
-            lst.append(arr[i])
-
-            if sum(lst) == target:
+            s_lst += arr[i]
+            if s_lst == target:
                 if i == 0:
-                    prefix[i] = len(lst)
+                    prefix[i] = 1
                 else:
-                    prefix[i] = min(prefix[i-1], len(lst))
+                    prefix[i] = min(prefix[i-1], i-0+1)
+            elif s_lst + target in diction2.keys():
+                prefix[i] = min(prefix[i-1], diction2[s_lst + target]-i)            
             elif i!= 0: #if i=0, then default
                 prefix[i] = prefix[i-1]
-        prefix = [len(arr)+1] + prefix[:-1]
+        prefix = [float('inf')] + prefix[:-1]
         
+        suffix = [float('inf')] * len(arr)
         lst = []
-        suffix = [len(arr)+1] * len(arr)
+        s_lst = 0
         for i in range(len(arr)-1, 0, -1): 
-            while lst and  sum(lst)+arr[i] > target:
-                lst.pop(0)
-            lst.append(arr[i])
-
-            if sum(lst) == target:
+            s_lst += arr[i]
+            if s_lst == target:
                 if i == len(arr)-1:
-                    suffix[i] = len(lst)
+                    suffix[i] = 1
                 else:
-                    suffix[i] = min(suffix[i+1], len(lst))
-            elif i != len(arr)-1:
+                    suffix[i] = min(suffix[i+1], i-0+1)
+            elif s_lst - target in diction.keys():
+                if i == len(arr)-1:
+                    suffix[i] =  i-diction[s_lst - target]+1
+                else:
+                    suffix[i] = min(suffix[i+1], i-diction[s_lst - target]+1)            
+            elif i!= len(arr)-1: #if i=0, then default
                 suffix[i] = suffix[i+1]
 
-        print("prefix")
-        print(prefix)
-        print("suffix")
-        print(suffix)
         two_arrays = [prefix[i] + suffix[i] for i in range(len(suffix))]
         if min(two_arrays) > len(arr):
             return -1
