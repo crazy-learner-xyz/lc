6411cf33034012f8f13aba1d1f7551f893acd38b
crazy-learner-xyz <73959442+crazy-learner-xyz@users.noreply.github.com>
2020-12-07 08:18:23 -0800

    Update solution.py

--- 0528. Random Pick with Weight/solution.py
+++ 0528. Random Pick with Weight/solution.py
@@ -1,1 +1,23 @@
+import random
 class Solution(object):
+
+    def __init__(self, w):
+        """
+        :type w: List[int]
+        """
+        self.lst= []
+        for i in range(len(w)):
+            self.lst += [i]*w[i]
+        
+
+    def pickIndex(self):
+        """
+        :rtype: int
+        """
+        return random.choice(self.lst)
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
